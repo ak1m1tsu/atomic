325917f46052ad2e55d72c9889b6bb172c8db47e
Roman Kravchuk <kravchuk.roman02@gmail.com>
2023-07-06 12:59:56 +0800

    refactor: sl.Err

--- internal/lib/logger/sl/sl.go
+++ internal/lib/logger/sl/sl.go
@@ -3,8 +3,8 @@ package sl
 import "golang.org/x/exp/slog"
 
 func Err(err error) slog.Attr {
-	return slog.String(
-		"error",
-		err.Error(),
-	)
+	return slog.Attr{
+		Key:   "error",
+		Value: slog.StringValue(err.Error()),
+	}
 }
