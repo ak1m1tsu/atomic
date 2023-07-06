f9a26868806df205b8ed336e9cd187a222162e63
Roman Kravchuk <kravchuk.roman02@gmail.com>
2023-07-06 12:56:52 +0800

    feat: implement alias storage

--- go.mod
+++ go.mod
@@ -3,14 +3,20 @@ module github.com/romankravchuk/atomic
 go 1.20
 
 require (
+	github.com/go-chi/chi/v5 v5.0.8
 	github.com/ilyakaznacheev/cleanenv v1.4.2
 	github.com/lib/pq v1.10.9
 	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
+	golang.org/x/oauth2 v0.10.0
 )
 
 require (
 	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
---
+++ internal/storage/postgresql/alias/storage.go
@@ -0,0 +1,73 @@
+package alias
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/lib/pq"
+	"github.com/romankravchuk/atomic/internal/data"
+	"github.com/romankravchuk/atomic/internal/storage"
+	"github.com/romankravchuk/atomic/internal/storage/postgresql"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) (*Storage, error) {
+	const op = "storage.postgresql.alias.New"
+
+	if db == nil {
+		return nil, fmt.Errorf("%s: %w", op, storage.ErrInvalidArgument)
+	}
+
+	return &Storage{
+		db: db,
+	}, nil
+}
+
+func (s *Storage) SaveAlias(url *data.Alias) error {
+	const (
+		op    = "storage.postgresql.alias.SaveURL"
+		query = "INSERT INTO url (url, alias) VALUES ($1, $2) RETURNING id"
+	)
+
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRow(url.URL, url.Name).Scan(&url.ID)
+	if err != nil {
+		if psqlErr, ok := err.(*pq.Error); ok && psqlErr.Code == postgresql.UniqueViolation {
+			return fmt.Errorf("%s: %w", op, storage.ErrAliasExists)
+		}
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (s *Storage) DeleteAlias(alias string) error {
+	const (
+		op    = "storage.postgresql.alias.DeleteURL"
+		query = "DELETE FROM url WHERE alias = $1"
+	)
+
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(alias)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return fmt.Errorf("%s: %w", op, storage.ErrAliasNotFound)
+		}
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
